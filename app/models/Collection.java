387efbabd92101e60c32e6c59072d86a025e16c7
Alan Accurso <accurso.alan@gmail.com>
2013-07-14 15:39:22 -0300

    Adds Seller, Collection and Item empty models.

---
+++ app/models/Collection.java
@@ -0,0 +1,9 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.db.ebean.Model;
+
+@Entity
+public class Collection extends Model {
+}
---
+++ app/models/Item.java
@@ -0,0 +1,8 @@
+package models;
+import java.util.*;
+import javax.persistence.*;
+import play.db.ebean.Model;
+
+@Entity
+public class Item extends Model {
+}
