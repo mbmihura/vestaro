7a559096d1fe933d6b43ae5de6d8dcf88422c3f4
Alan Accurso <accurso.alan@gmail.com>
2013-07-14 18:48:55 -0300

    Adds class SellerPoint and view sellerItem, which creates an <ul> composed of a seller items.

---
+++ app/models/SellerPoint.java
@@ -0,0 +1,19 @@
+package models;
+
+import org.joda.time.DateTime;
+import play.data.format.Formats;
+import play.db.ebean.Model;
+
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class SellerPoint extends Model {
+    @Id
+    public Long id;
+    public Seller seller;
+    @Formats.DateTime(pattern="yyyy/MM/dd HH:mm:SS")
+    public DateTime insertDate;
+    public BigDecimal pointsValue;
+    public Integer itemCount;
+}
