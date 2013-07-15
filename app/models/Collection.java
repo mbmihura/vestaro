d22d302e6b1d5d19caff269217852abb13a8edd9
Alan Accurso <accurso.alan@gmail.com>
2013-07-15 00:25:22 -0300

    Adds Collection initial attributes.

--- app/models/Collection.java
+++ app/models/Collection.java
@@ -2,8 +2,26 @@ package models;
 
 import java.util.*;
 import javax.persistence.*;
+import play.data.format.Formats;
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
 @Entity
 public class Collection extends Model {
+    @Id
+    public Long id;
+
+    @Constraints.Required
+    @Formats.NonEmpty
+    public String title;
+
+    public String description;
+
+    @OneToOne
+    public Seller owner;
+
+    @OneToMany
+    public List<Item> items = new ArrayList<>();
+
+    public static Finder<Long,Collection> find = new Finder(Long.class,Collection.class);
 }
