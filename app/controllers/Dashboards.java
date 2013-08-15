ced2e616a52ec858278572125f789d6424a7a94c
Martin Mihura <mbmihura@gmail.com>
2013-08-15 12:43:13 -0300

    Creates horizontal list to display items.

--- app/controllers/Dashboards.java
+++ app/controllers/Dashboards.java
@@ -3,11 +3,12 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.dashboards.*;
+import views.html.shared.*;
 
 public class Dashboards extends Controller {
   
     public static Result index() {
-        return ok(customer.render());
+        return ok(itemListHorizontal.render());
     }
 
     
