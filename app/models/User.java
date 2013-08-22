35e309d07602a6f27bb4f99bdf5fbdd674ec962a
Martin Mihura <mbmihura@gmail.com>
2013-08-22 17:10:26 -0300

    Creates User model to be used with authentication and fbLogin.

---
+++ app/models/User.java
@@ -0,0 +1,34 @@
+package models;
+
+import javax.persistence.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+
+@Entity
+public class User extends Model {
+
+    @Id
+    public Long userId;
+    public String name;
+    
+    public User(Long userId, String name) {
+      this.userId = userId;
+      this.name = name;
+    }
+
+    public static Finder<Long,User> find = new Finder<Long,User>(
+        Long.class, User.class
+    ); 
+
+    public static User authenticate(Long userId) {
+        return find.where().eq("userId", userId)
+            .findUnique();
+    }
+
+    public static User create(Long userId, String name)
+    {
+      User newUser = new User(userId, name);
+      newUser.save();
+      return newUser;
+    }
+}
