7b8cb58575a3a62b07ec26ecb52c36df06bdaa78
Martin Mihura <mbmihura@gmail.com>
2013-09-06 16:34:37 -0300

    Fb's login data send by client is validate using signedRequest and appSecretToken (to make sure it was created by Fb) before user is logIn and session created.

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -5,7 +5,14 @@ import play.mvc.Result;
 import play.data.Form;
 import play.libs.Json;
 import models.FbAuthResponse;
+import utils.Crypto;
 import views.html.index;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
+
+
 
 public class Authentication extends Controller {
   
@@ -13,20 +20,40 @@ public class Authentication extends Controller {
         Form<FbAuthResponse> fbAuthResponseForm = Form.form(FbAuthResponse.class);
         FbAuthResponse fbAuth = fbAuthResponseForm.bindFromRequest().get();
 
-    	if (true) //TODO: Fadebook validation & token
-    	{
-			session().clear();
-        	session("userId", fbAuth.userID);
-        	return ok(Json.toJson(fbAuth));
-    	} else {
-    		return badRequest("Invalid user");
-    	}
-    }  
+        String signedRequest = fbAuth.signedRequest;
+
+        if (signedRequest != null && !signedRequest.isEmpty())
+        { 
+            String[] signedRequests = signedRequest.split("\\.", 2);
+            String urlBase64EncodedSignature = signedRequests[0];
+            String payload = signedRequests[1];
+
+            String signature = Crypto.getBase64UrlDecode(urlBase64EncodedSignature);
+            String expectedSignature = Crypto.getHashHmacSHA256(payload,"4f544dfc621106bd86c30f2ce14d2575");
+
+            //Verified that computed signature and recieved one matches.
+            if(expectedSignature.equals(signature)) {
+                String signedData = Crypto.getBase64UrlDecode(payload);
+
+                // Retrieve userId and create a session for it.
+                String userId = Json.parse(signedData).get("user_id").toString();
+                session().clear();
+                session("userId", userId);
+            
+                return ok(Json.parse(signedData));
+            } else {
+                return forbidden();
+            }
+        } else
+        {
+            return badRequest("SignedRequest is requiered and its value was " + signedRequest);
+        }
+	}
 
     public static Result logout() {
-	    session().clear();
-	    return redirect(
-	        routes.Application.index()
-	    );
-	}
+        session().clear();
+        return redirect(
+            routes.Application.index()
+        );
+    }
 }
---
+++ app/utils/Crypto.java
@@ -0,0 +1,74 @@
+package utils;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+
+/**
+ * Crypto utilities.
+ *
+ * @author    Martin Mihura
+ * @version   Release: 1.0.0
+ */
+public class Crypto {
+
+    /**
+     * Utility classes should not have a public or default constructor.
+     */
+    private Crypto() {
+        throw new AssertionError();
+    }
+
+    /**
+     * Retrieves the base64 for Url decode string for the given input.
+     * @param urlBase64Encoded The string to decoded.
+     * @return The decoded string.
+     */
+    public static String getBase64UrlDecode(final String urlBase64Encoded) {
+        //Add '=' padding char, and replace '-' with '+' and '_' with '/' to get the signature's URLBase64 encoded version from the URLBase64 encoded one.
+        String padding = "";
+        switch (urlBase64Encoded.length() % 4) {
+             case 0:
+                 break;
+             case 1:
+                 padding = "===";
+                 break;
+             case 2:
+                 padding = "==";
+                 break;
+             default:
+                 padding = "=";
+        }
+        String base64EncodedSignature = urlBase64Encoded.replace("-", "+").replace("_", "/").trim() + padding;
+        try {
+           return new String(Base64.decodeBase64(base64EncodedSignature.getBytes("UTF-8")), "UTF-8");
+        } catch (final UnsupportedEncodingException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Retrieves the given input string hashed with HmacSHA256 algorithm.
+     * @param data String to be hash.
+     * @param secret scretkey used to hash the input data.
+     * @return The resulting hash.
+     */
+    public static String getHashHmacSHA256(final String data, final String secret) {
+        //Compute HMAC-SHA256 hash signature from the data recieved using secret code.
+        try {
+            final Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256");
+            sha256_HMAC.init(secretKey);
+            byte[] hash = sha256_HMAC.doFinal(data.getBytes("UTF-8"));
+            return new String(hash,"UTF-8");
+        } catch (final NoSuchAlgorithmException | UnsupportedEncodingException | InvalidKeyException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
