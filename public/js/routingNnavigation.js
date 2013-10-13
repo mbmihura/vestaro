b68f49e72251edd6e9908d518f2f561dc467bcb6
Martin Mihura <mbmihura@gmail.com>
2013-10-13 20:58:32 -0300

    When angular's routes provider cannot find an angular's template-controller for the requested url, the page request to the server which will return a html partial-view or a 404 page.

--- public/js/routingNnavigation.js
+++ public/js/routingNnavigation.js
@@ -1,10 +1,18 @@
 angular.module('vestaroMain', ['firebase']).
   config(function($routeProvider) {
+	  
+	  var template = '<div ng-include="templateUrl">Loading...</div>';
+	  
 	  $routeProvider.
       when('/', {controller:BuyerHomeCtrl, templateUrl:'assets/html/buyerHome.html'}).
       when('/dashboard', {controller:SellerDashboardCtrl, templateUrl:'assets/html/sellerDashboard.html'}).
+      when('/:serverPageUrl', {template: template, controller: 'serverPageRoutingCtrl'}).
       otherwise({redirectTo:'/'});
   });
+
+angular.module('vestaroMain').controller('serverPageRoutingCtrl', ['$scope', '$routeParams' ,function($scope, $routeParams){
+	  $scope.templateUrl = "/" + $routeParams.serverPageUrl;
+	}]);
 
 function BuyerHomeCtrl($scope) {
   $scope.projects = [{name: "a", description: "desc"}, {name:"b", description:"desc 2"}];
