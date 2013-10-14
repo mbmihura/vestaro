d29fa838588b2579b6e3cd3e333ea149bbd1d085
Alan Accurso <accurso.alan@gmail.com>
2013-10-14 00:55:01 -0300

    Adds Navigation Controller.

---
+++ public/js/routingNavigation.js
@@ -0,0 +1,37 @@
+angular.module('vestaroMain', []).
+  config(function($routeProvider) {
+	  
+	  var template = '<div ng-include="templateUrl">Loading...</div>';
+	  
+	  $routeProvider.
+      when('/', {controller:BuyerHomeCtrl, templateUrl:'assets/html/buyerHome.html'}).
+      when('/dashboard', {controller:SellerDashboardCtrl, templateUrl:'assets/html/sellerDashboard.html'}).
+      when('/:serverPageUrl', {template: template, controller: 'serverPageRoutingCtrl'}).
+      otherwise({redirectTo:'/'});
+  });
+
+angular.module('vestaroMain')
+	.controller('serverPageRoutingCtrl', ['$scope', '$routeParams' ,function($scope, $routeParams){
+		$scope.templateUrl = "/" + $routeParams.serverPageUrl;
+	}])
+	.controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
+	    $scope.views = [
+	      {path: '/', title: 'Inicio', icon: 'home'},
+	      {path: '/wishlist', title: 'Wishlist', icon: 'star'},
+	      {path: '/buyerProfile', title: 'Perfil', icon: 'user'},
+	      {path: '/itemSearch', title: 'Prendas', icon: 'th'},
+	    ];
+	    $scope.isActive = function(view) {
+	      if (view.path == $location.path()) {
+	        return true;
+	      }
+	      return false;
+	    };
+  }]);
+
+function BuyerHomeCtrl($scope) {
+  $scope.projects = [{name: "a", description: "desc"}, {name:"b", description:"desc 2"}];
+}
+ 
+function SellerDashboardCtrl($scope) {
+}
