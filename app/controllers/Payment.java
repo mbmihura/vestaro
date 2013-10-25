a63cc0be5f5a6529b11885a73d52993ed54b28e6
natponzo <natasha.ponzo@gmail.com>
2013-10-24 21:21:23 -0300

    successful payment page

--- app/controllers/Payment.java
+++ app/controllers/Payment.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import models.BuyOrder;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Payment extends Controller{
 
 	public static Result success(Long orderId){
-		return TODO;
+		BuyOrder buyOrder= BuyOrder.find.byId(orderId);
+		buyOrder.successfulPayment();
+		return ok(views.html.successfulPayment.render(buyOrder, (buyOrder.pointsEarned >0)));
+
 		
 	}
 	public static Result error(Long orderId){
