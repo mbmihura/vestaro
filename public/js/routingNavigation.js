c76cf9878db1a356da3abfe70777a6fc3cac4010
Martin Mihura <mbmihura@gmail.com>
2013-10-27 15:43:44 -0300

    Angular's  replaces 404 response to its request, with a 200 and a 'not Found Page'.

--- public/js/routingNavigation.js
+++ public/js/routingNavigation.js
@@ -54,6 +54,46 @@ vestaroMain.config(function($routeProvider) {
     
 }]);
 
+vestaroMain
+    .config(['$httpProvider', function ($httpProvider) {
+
+    var $http,
+        interceptor = ['$q', '$injector', function ($q, $injector) {
+
+            function success(response) {
+                return response;
+            }
+
+            function error(response) {
+                if (response.status === 404 && response.config.url.indexOf(".html")) {
+
+                    // get $http via $injector because of circular dependency problem
+                    $http = $http || $injector.get('$http');
+
+                    var defer = $q.defer();
+                    $http.get('/assets/html/server/404NotFound.html')
+                        .then(function (result) {
+                            response.status = 200;
+                            response.data = result.data;
+                            defer.resolve(response);
+                        }, function () {
+                            defer.reject(response);
+                        });
+
+                    return defer.promise;// response;
+                } else {
+                    return $q.reject(response);
+                }
+            }
+
+            return function (promise) {
+                return promise.then(success, error);
+            }
+        }];
+
+    $httpProvider.responseInterceptors.push(interceptor);
+}]);
+
 /* Filters */
 vestaroMain.filter('priceBetween', function () {
     return function ( items, minPrice, maxPrice ) {
