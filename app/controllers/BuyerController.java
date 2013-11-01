6d8a1fd600ffb67ba662dd731a9fc80bb3718f45
natponzo <natasha.ponzo@gmail.com>
2013-11-01 20:49:21 -0300

    mockorders deleted + showing real buyer orders

--- app/controllers/BuyerController.java
+++ app/controllers/BuyerController.java
@@ -1,36 +1,20 @@
 package controllers;
 
-import java.util.ArrayList;
-import controllers.BaseController;
-
 import models.BuyOrder;
-import models.BuyOrder.State;
 import models.Buyer;
-import models.Item;
-import models.Seller;
-import play.mvc.Controller;
 import play.mvc.Result;
-import views.*;
 
 public class BuyerController extends BaseController {
   
     public static Result profile(){
     	
-    	return ok(views.html.buyerProfile.render(getMockOrders(), Buyer.findBuyerByUser( currentUserId())));
+    	
+    	Buyer buyer = Buyer.findBuyerByUser(currentUserId());
+    	return ok(views.html.buyerProfile.render(BuyOrder.findBuyerOrders(buyer.id), buyer));
     }
 	
 
-	private static ArrayList<BuyOrder> getMockOrders() {
-		ArrayList<BuyOrder> orders = new ArrayList<BuyOrder>();
-		
-    	Item item1= Item.find.byId("CG1");
-    	Item item2= Item.find.byId("CB1");
-    	orders.add(new BuyOrder((long) 1,item1, new models.Buyer(), "L", 55, State.PAYMENT_PENDING));
-    	orders.add(new BuyOrder((long) 2,item2, new models.Buyer(), "M", 50, State.RECEPTION_PENDING));
-    	orders.add(new BuyOrder((long) 3,item1, new models.Buyer(), "S", 55, State.RECEPTION_CONFIRMED));
-    	orders.add(new BuyOrder((long) 4,item1, new models.Buyer(), "S", 50, State.IN_DISPUTE));
-		return orders;
-	}
+	
     
 }
 
