48c71eaf2470f2dc66c2bc5f35d1cae6169ca1f3
Martin Mihura <mbmihura@gmail.com>
2013-11-05 01:19:18 -0300

    Add user delete and list endpoint for registration testing purpose.

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,12 +1,19 @@
 package controllers;
 
+import org.codehaus.jackson.JsonNode;
+
 import play.Play;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.libs.Json;
+import models.Buyer;
+import models.Rol;
+import models.Seller;
 import models.User;
+import security.RestrictTo;
+import security.Roles;
 import security.SubjectPresent;
 import utils.Crypto;
 
@@ -31,9 +38,33 @@ public class UserController extends BaseController {
     public static Result readCurrentId() {
         return ok(Json.toJson(currentUserId()));
     }
-    
+  
     @SubjectPresent
     public static Result readCurrent() {
         return ok(Json.toJson(currentUser()));
+    }
+    
+    @RestrictTo(Roles.ADMIN)
+    public static Result readAll() {
+        return ok(Json.toJson(User.find.all()));
+    }
+    
+    @RestrictTo(Roles.ADMIN)
+    public static Result delete(Long id) {
+    	// TODO validate that current user is not been delete.
+    	User user = User.findById(id);
+    	JsonNode userJson = Json.toJson(user);
+    	if (user == null) 
+    		return notFound();
+    	else {
+    		user.deleteManyToManyAssociations("roles");
+	    	Seller seller = Seller.findSellerByUser(id);
+	    	if (seller != null) seller.delete();
+	    	Buyer buyer = Buyer.findBuyerByUser(id);
+	    	if (buyer != null) buyer.delete();
+	    	
+	    	user.delete();
+	        return ok(userJson);
+    	}
     }
 }
