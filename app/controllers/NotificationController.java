09e05a05bacad7c8e021891e95bf6361ee138775
natponzo <natasha.ponzo@gmail.com>
2013-11-14 22:53:01 -0300

    showing notifications (without user photo)

--- app/controllers/NotificationController.java
+++ app/controllers/NotificationController.java
@@ -1,10 +1,16 @@
 package controllers;
 
+import java.util.List;
+import java.util.Map;
+
+import models.Notification;
+import models.Seller;
 import play.mvc.Result;
 
 public class NotificationController extends BaseController {
 	public static Result getNotifications() {
-
-		return ok(views.html.notifications.render());
+		Map<String, List<Notification>> notifications = Notification
+				.getNotifications(Seller.findSellerByUser(currentUserId()).id);
+		return ok(views.html.notifications.render(notifications));
 	}
 }
--- app/models/Notification.java
+++ app/models/Notification.java
@@ -1,5 +1,12 @@
 package models;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
@@ -10,40 +17,112 @@ import play.db.ebean.Model;
 @Entity
 public class Notification extends Model {
 
-	public enum NOTIFICATION_TYPE {
-		SALE, DISPUTE
+	public enum NotificationType {
+		SALE("Nueva Venta", " compró el item "), DISPUTE("Venta en Disputa",
+				" abrió una disputa por el item ");
+
+		private String message;
+		private String action;
+
+		public String getMessage() {
+			return message;
+		}
+
+		private NotificationType(String msg, String action) {
+			this.message = msg;
+			this.action = action;
+		}
+
+		public String getAction() {
+			return action;
+		}
+
 	}
 
 	@Id
 	public Long id;
 
 	@OneToOne
-	public BuyOrder buyOrder;
-	public NOTIFICATION_TYPE type;
+	public NotificationType notificationType;
 	public boolean seen;
+	public String buyerName;
+	public String itemTitle;
+	public Long sellerId;
+
+	public Date create_time;
+
+	public static Finder<Long, Notification> find = new Finder<Long, Notification>(
+			Long.class, Notification.class);
 
 	public Notification() {
 
 	}
 
-	public Notification(BuyOrder order, NOTIFICATION_TYPE type) {
-		this.type = type;
-		this.buyOrder = order;
+	public Notification(BuyOrder order, NotificationType type) {
+		this.notificationType = type;
 		this.seen = false;
+		this.buyerName = order.buyer.user.name;
+		this.itemTitle = order.item.title;
+		this.sellerId = order.item.seller.id;
 
 	}
 
-	public void createNotification(BuyOrder order, NOTIFICATION_TYPE type) {
+	public void createNotification(BuyOrder order, NotificationType type) {
 		Notification newNotification = new Notification(order, type);
+		newNotification.create_time = new Date();
 		newNotification.save();
 
 	}
 
 	public void notifyDispute(BuyOrder order) {
-		createNotification(order, NOTIFICATION_TYPE.DISPUTE);
+		createNotification(order, NotificationType.DISPUTE);
 	}
 
 	public void notifySale(BuyOrder order) {
-		createNotification(order, NOTIFICATION_TYPE.SALE);
+		createNotification(order, NotificationType.SALE);
+	}
+
+	public static Map<String, List<Notification>> getNotifications(Long sellerId) {
+		List<Notification> notificationList = Notification.find.where()
+				.eq("sellerId", sellerId).orderBy("create_time DESC")
+				.findList();
+
+		Map<String, List<Notification>> groupByNotifications = groupByNotifications(notificationList);
+		return groupByNotifications;
+
+	}
+
+	private static Map<String, List<Notification>> groupByNotifications(
+			List<Notification> notificationList) {
+
+		Map<String, List<Notification>> groupByNotifications = new LinkedHashMap<String, List<Notification>>();
+		if (notificationList.size() == 0) {
+			return groupByNotifications;
+		}
+
+		String stringDate = stringFormatDate(notificationList.get(0).create_time);
+		ArrayList<Notification> notifications = new ArrayList<>();
+
+		for (Notification notification : notificationList) {
+			if (stringFormatDate(notification.create_time).equalsIgnoreCase(
+					stringDate)) {
+				notifications.add(notification);
+			} else {
+				groupByNotifications.put(stringDate, notifications);
+
+				stringDate = stringFormatDate(notification.create_time);
+				notifications = new ArrayList<Notification>();
+			}
+		}
+		groupByNotifications.put(stringDate, notifications);
+
+		return groupByNotifications;
+	}
+
+	private static String stringFormatDate(Date date) {
+		String month = new SimpleDateFormat("MMM").format(date);
+		String day = new SimpleDateFormat("dd").format(date);
+		String stringDate = day + " de " + month;
+		return stringDate;
 	}
 }
