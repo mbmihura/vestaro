307919a1714d9bd83e33b646351ff1ce3709fea5
Martin Mihura <mbmihura@gmail.com>
2013-11-14 21:03:23 -0300

    Add catch for 401 and 403 responses.

--- public/js/app.js
+++ public/js/app.js
@@ -15,23 +15,33 @@ vestaroMain.config(['$httpProvider', function ($httpProvider) {
 
             function error(response) {
               //TODO: replace with angular new pattern
-                if (response.status === 404 && response.config.url.indexOf(".html")) {
-
-                    // get $http via $injector because of circular dependency problem
-                    $http = $http || $injector.get('$http');
-                    var defer = $q.defer();
-                    $http.get('/assets/html/server/404NotFound.html')
+              switch (errorResponse.status) {
+                case 401:
+                    //TODO
+                    break;
+                case 403:
+                    //TODO
+                    break;
+                case 404:
+                    if (response.config.url.indexOf(".html"))
+                    {
+                      // get $http via $injector because of circular dependency problem
+                      $http = $http || $injector.get('$http');
+                      var defer = $q.defer();
+                      $http.get('/assets/html/server/404NotFound.html')
                         .then(function (result) {
-                            response.status = 200;
-                            response.data = result.data;
-                            defer.resolve(response);
+                          response.status = 200;
+                          response.data = result.data;
+                          defer.resolve(response);
                         }, function () {
-                            defer.reject(response);
-                        });
-
-                    return defer.promise;// response;
-                if (response.status === 500 && response.config.url.indexOf(".html")) {
-
+                          defer.reject(response);
+                          });
+                      return defer.promise;// response;
+                    } else {
+                      return $q.reject(response);
+                    }
+                    break;
+                case 500:
                     // get $http via $injector because of circular dependency problem
                     $http = $http || $injector.get('$http');
                     var defer = $q.defer();
@@ -45,9 +55,10 @@ vestaroMain.config(['$httpProvider', function ($httpProvider) {
                         });
 
                     return defer.promise;// response;
-                } else {
+                    break;
+                default:
                     return $q.reject(response);
-                }
+              }
             }
 
             return function (promise) {
