fe67b1bfdf1e8f3d1a3667841a2089ba0202ed99
nadalpablo <Pablo@Pablo-PC>
2013-11-18 12:22:40 -0300

    Fixes history chart from dashboard and adds some mock actions in yml

--- public/js/sellerDashboards/dashboard.js
+++ public/js/sellerDashboards/dashboard.js
@@ -1,6 +1,3 @@
-var chartData = [];
-var chart;
-   
 Number.prototype.padLeft =
 function(base, chr){
    var len = (String(base || 10).length - String(this).length) + 1;
@@ -194,6 +191,9 @@ jsRoutes.controllers.DashboardController.littleItemsStock().ajax({success:
 	}
 });
 
+var chartData = [];
+var chart;
+
 function zoomChart() {
     chart.zoomToIndexes(chartData.length - 40, chartData.length - 1);
 }
@@ -205,49 +205,43 @@ function generateChartData(json) {
             actions_count: json[i].actions_count
         });
 	}
-    
-    return chartData;
+	
+	return chartData;
 }
     
 // ==================== History boughts ====================
 jsRoutes.controllers.ActionController.getAllTimeActions("BUY").ajax({success: 
 	function(json)
-	{
-	        chart = new AmCharts.AmSerialChart();
-	        chart.pathToImages = "/assets/img/";
-	        chart.dataProvider = generateChartData(json);
-	        chart.categoryField = "date";
-	
-	        chart.addListener("dataUpdated", zoomChart);
-	
-	        var categoryAxis = chart.categoryAxis;
-	        categoryAxis.parseDates = true;			 
-	        categoryAxis.gridAlpha = 0.07;
-	        categoryAxis.axisColor = "#DADADA";
-	
-	        var valueAxis = new AmCharts.ValueAxis();
-	        valueAxis.gridAlpha = 0.07;
-	        chart.addValueAxis(valueAxis);
-	
-	        var graph = new AmCharts.AmGraph();
-	        graph.type = "line";
-	        graph.title = "red line";
-	        graph.valueField = "actions_count";
-	        graph.lineAlpha = 1;
-	        graph.lineColor = "#d1cf2a";
-	        graph.fillAlphas = 0.3;
-	        chart.addGraph(graph);
-	
-	        // CURSOR
-	        var chartCursor = new AmCharts.ChartCursor();
-	        chartCursor.cursorPosition = "mouse";
-	        chartCursor.categoryBalloonDateFormat = "DD/MM/YYYY";
-	        chart.addChartCursor(chartCursor);
-	
-	        // SCROLLBAR
-	        var chartScrollbar = new AmCharts.ChartScrollbar();
-	        chart.addChartScrollbar(chartScrollbar);
-	
-	        chart.write("chartdiv_history");
+	{	
+	    // SERIAL CHART
+	    chart = new AmCharts.AmSerialChart();
+	    chart.pathToImages = "/assets/img/";
+	    chart.dataProvider = generateChartData(json);
+	    chart.categoryField = "date";
+	    chart.addListener("dataUpdated", zoomChart);
+	    var categoryAxis = chart.categoryAxis;
+	    categoryAxis.parseDates = true;
+	    categoryAxis.minPeriod = "DD";		 
+	    categoryAxis.gridAlpha = 0.07;
+	    categoryAxis.axisColor = "#DADADA";
+	    var valueAxis = new AmCharts.ValueAxis();
+	    valueAxis.gridAlpha = 0.07;
+	    chart.addValueAxis(valueAxis);
+	    var graph = new AmCharts.AmGraph();
+	    graph.type = "line";
+	    graph.title = "red line";
+	    graph.valueField = "actions_count";
+	    graph.lineAlpha = 1;
+	    graph.lineColor = "#d1cf2a";
+	    graph.fillAlphas = 0.3;
+	    chart.addGraph(graph);
+	    var chartCursor = new AmCharts.ChartCursor();
+	    chartCursor.cursorPosition = "mouse";
+	    chartCursor.categoryBalloonDateFormat = "DD/MM/YYYY";
+	    chart.addChartCursor(chartCursor);
+	    var chartScrollbar = new AmCharts.ChartScrollbar();
+	    chart.addChartScrollbar(chartScrollbar);
+	    
+	    chart.write("chartdiv_history");
 	}
 });
